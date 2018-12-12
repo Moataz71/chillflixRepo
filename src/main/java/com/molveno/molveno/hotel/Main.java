2625dab4196ca339f9a0e57ece16174996a3e303
msheikhalard <Miky_Mike123>
2018-12-12 13:40:17 +0100

    last changes before Hans's visit at 12-12-18

--- src/main/java/com/molveno/molveno/hotel/Main.java
+++ src/main/java/com/molveno/molveno/hotel/Main.java
@@ -8,10 +8,7 @@ import com.molveno.molveno.hotel.Booking.Booking;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -45,60 +42,36 @@ public class Main {
 
         date3 = LocalDate.parse(checkOut);
 
+        while (date3.isEqual(date2) || date3.isBefore(date2)) {
+            System.out.println("Enter the Correct Check-Out Date: (yyyy-MM-dd) ");
+            checkOut = scanner.nextLine();
+            date3 = LocalDate.parse(checkOut);
+        }
+
         booking.setCheckOut(date3);
 
         System.out.println("Guest Count: ");
         int guestCount = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Rooms are available:" + "\n" + "*Budget = 80 Euro" + "\n" + "*Standard = 100 Euro" + "\n" + "*Lux = 140 Euro");
-        System.out.println("Enter your Choice: ");
-        String roomChoice = scanner.nextLine();
-        System.out.println("Enter your full name: ");
-        String name = scanner.nextLine();
-        System.out.println("Email: ");
-        String email = scanner.nextLine();
-
-        System.out.println("Phone Number: ");
-        String phoneNumber = scanner.nextLine();
-
-        System.out.println("Address: ");
-        String address = scanner.nextLine();
-
-        System.out.println("Country: ");
-        String country = scanner.nextLine();
-
-        System.out.println("Nationality: ");
-        String nationality = scanner.nextLine();
-
-        Guest guestInfo = new Guest();
-        guestInfo.setName(name);
-        guestInfo.setEmail(email);
-        guestInfo.setPhone(phoneNumber);
-        guestInfo.setAddress(address);
-        guestInfo.setCountry(country);
-        guestInfo.setNationality(nationality);
-
-        Date date = new Date();
-        // display time and date using toString()
-        System.out.println(date.toString());
 
 
-        LocalDate BookingRequest = LocalDate.of(2018, Month.DECEMBER, 05);
-        System.out.println("ReservationRequest is " + BookingRequest);
+        LocalDate BookingRequest = LocalDate.of(2018, Month.DECEMBER, 12);
+
+        System.out.println("Reservation Request is at the date: " + BookingRequest);
 
 
-        LocalDate checkInDate = LocalDate.of(2018, Month.DECEMBER, 05);
+        LocalDate checkInDate = LocalDate.of(2018, Month.DECEMBER, 12);
         //LocalDate Booking1 = LocalDate.of(checkInDate);
 
-        LocalDate checkOutDate = LocalDate.of(2018, Month.DECEMBER, 06);
+        LocalDate checkOutDate = LocalDate.of(2018, Month.DECEMBER, 16);
 
         //LocalDate Booking2 = LocalDate.of(checkOutDate);
 
-        List<LocalDate> current = new ArrayList<>();
+//        List<LocalDate> current = new ArrayList<>();
         //current.add(Booking1);
         //current.add(Booking2);
 
-        System.out.println("current is  " + current);
+//        System.out.println("current is  " + current);
 
         //System.out.println(ReservationLogic.checkDateTime(ReservationRequest, current));
 
@@ -122,22 +95,111 @@ public class Main {
         rooms.add(room3);
         rooms.add(room4);
 
-        System.out.println(rooms);
 
-        Booking rese1= new Booking();
-        rese1.setRoom(room1);
-       // rese1.setCheckIn(Booking1);
+        Map<String, Integer> proMap = new HashMap<>();
+        proMap.put(" * Budget ", 80);
+        proMap.put(" * Standard  ", 100);
+        proMap.put(" * Luxury  ", 140);
 
-        Booking rese2= new Booking();
-        rese2.setRoom(room2);
-        //rese2.setCheckIn(Booking2);
-        List<Booking> listBookings = new ArrayList<>();
+        List<Integer> professionalList = new ArrayList<>();
+        Iterator it = proMap.entrySet().iterator();
 
-        listBookings.add(rese1);
-        listBookings.add(rese2);
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            professionalList.add((Integer) pair.getValue());
+        }
+
+        for (Map.Entry entry : proMap.entrySet()) {
+            System.out.println("Rooms are available: \n" + rooms.size() + entry);
+        }
+
+        System.out.println("Enter your Choice: ");
+        String roomChoice = scanner.nextLine();
+        System.out.println("You choose this room: " + roomChoice);
+
+        String decision;
+
+        boolean yn = true;
+        while (yn) {
+            System.out.println("Would you like to register and create a profile? (yes or no)");
+
+            decision = scanner.nextLine();
+
+            switch (decision) {
+                case "yes":
+                    yn = true;
+                    break;
+
+                case "no":
+                    yn = false;
+                    break;
+
+                default:
+                    System.out.println("please enter again ");
+                    boolean repeat = true;
+
+                    while (repeat) {
+                        System.out.println("enter another answer : yes or no");
+                        decision = scanner.nextLine();
+
+                        switch (decision) {
+                            case "yes":
+                                yn = repeat = true;
+                                break;
+
+                            case "no":
+                                yn = repeat = false;
+                                break;
+                        }
+                    }
+                    break;
+            }
+
+            System.out.println("Enter your full name: ");
+            String name = scanner.nextLine();
+
+            System.out.println("Email: ");
+            String email = scanner.nextLine();
+
+            System.out.println("Phone Number: ");
+            String phoneNumber = scanner.nextLine();
+
+            System.out.println("Address: ");
+            String address = scanner.nextLine();
+
+            System.out.println("Country: ");
+            String country = scanner.nextLine();
+
+            System.out.println("Nationality: ");
+            String nationality = scanner.nextLine();
 
 
+            Guest guestInfo = new Guest();
+            guestInfo.setName(name);
+            guestInfo.setEmail(email);
+            guestInfo.setPhone(phoneNumber);
+            guestInfo.setAddress(address);
+            guestInfo.setCountry(country);
+            guestInfo.setNationality(nationality);
+
+            Date date = new Date();
+            // display time and date using toString()
+            System.out.println(date.toString());
+
+            Booking rese1 = new Booking();
+            rese1.setRoom(room1);
+            // rese1.setCheckIn(Booking1);
+
+            Booking rese2 = new Booking();
+            rese2.setRoom(room2);
+            //rese2.setCheckIn(Booking2);
+            List<Booking> listBookings = new ArrayList<>();
+
+            listBookings.add(rese1);
+            listBookings.add(rese2);
+
+            break;
+        }
     }
+
 }
-
-
