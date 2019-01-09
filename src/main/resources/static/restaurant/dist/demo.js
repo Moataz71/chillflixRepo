8c9e969ef7b81acb13ecbd935086d9bc6a51d08e
MALKURDI <mohmmad.alkurdi@capgemini.com>
2019-01-09 15:36:31 +0100

    Final edtition for first user interface by moataz

--- src/main/resources/static/restaurant/dist/demo.js
+++ src/main/resources/static/restaurant/dist/demo.js
@@ -1,143 +1,37 @@
-
-//for spinner buttons  
-
-$(function ($) {
-       $('#handleCounter1').handleCounter({
-    minimum: 1,
-    maximize: 100,
-   
-           
-    })
-          
-        $('#handleCounter2').handleCounter({
-    minimum: 1,
-    maximize: 100,
-             
-           })
-       
-     
-       
-       });
-      
-   
-
-//for datepicker
-
-addEventListener('DOMContentLoaded', function () {
-	
-
-   
-    
-	pickmeup('.range', {
-		mode : 'range',
-        separator: '    to   ',
-        position :'bottom',
-//        hide_on_select : true,
-        format: 'd b-y',
-      
-	});
-//pickmeup('.range').show();
-
-
-    
-});
-
-
-// map
-function initMap() {
-        var uluru = {lat:  46.133264, lng: 10.968631};  
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 14,
-          center: uluru
+$('document').ready(function() {
+        $('.menu').each(function() {
+            $(this).css("display","none");});
+    $('#M1').click(function(){
+        $('.menu').each(function() {
+            $(this).css("display","none");});
+            $('#tabelM1').css("display","block");
         });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
+    $('#M2').click(function(){
+            $('.menu').each(function() {
+                $(this).css("display","none");});
+            $('#tabelM2').css("display","block");
         });
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// image slider main
-$(document).ready(function(){
-    
- 
-                $('.single').slick({
-                autoplay : true,
-                  pauseOnFocus : false,
-                    pauseOnHover : false,
-                 arrows : false,
-                    draggable : false,
-                         });
-              
-                $('.left').click(function(){
-                  $('.single').slick('slickPrev');
-                })
-
-                $('.right').click(function(){
-                  $('.single').slick('slickNext');
-                })
-                
-                
-                
-                 
-                $('.room').slick({
-                autoplay : false,
-                  slidesToShow :3,
-                  pauseOnFocus : true,
-                
-                 arrows : false,
-                    draggable : true,
-                 swipeToSlide : true,
-                  });
-    
-                
-                $('.review').slick({
-                    
-                autoplay : true,
-                    autoplaySpeed : 6000,
-                  pauseOnFocus : true,
-                 pauseOnHover : false,
-                 arrows : false,
-                    draggable : true,
-             
-                    
-                         });
-    
-    
-    
-    
-   
+    $('#M3').click(function(){
+            $('.menu').each(function() {
+                $(this).css("display","none");});
+            $('#tabelM3').css("display","block");
         });
+            $('#M4').click(function(){
+                    $('.menu').each(function() {
+                        $(this).css("display","none");});
+                    $('#tabelM4').css("display","block");
+                });
+                    $('#M5').click(function(){
+                            $('.menu').each(function() {
+                                $(this).css("display","none");});
+                            $('#tabelM5').css("display","block");
+                        });
 
 
 
-//scrolltotop
- 
-   $(function () {
-  $.scrollUp({
-    scrollName: 'scrollUp', // Element ID
-    topDistance: '600' , // Distance from top before showing element (px)
-    topSpeed: 300 , // Speed back to top (ms)
-    animation: 'fade', // Fade, slide, none
-    animationInSpeed: 500, // Animation in speed (ms)
-    animationOutSpeed: 500, // Animation out speed (ms)
-  scrollText: '^',
-    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-  });
-});
+})
+
+
+
+
+
