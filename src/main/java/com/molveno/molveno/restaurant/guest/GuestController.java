86ff4dc5ea9ec0456178013fa17d41fb2fea812d
SSHIEKH <samer-al.shiekh@capgemini.com>
2019-01-23 16:14:04 +0100

    Tables can be selected from the backend

---
+++ src/main/java/com/molveno/molveno/restaurant/guest/GuestController.java
@@ -0,0 +1,40 @@
+package com.molveno.molveno.restaurant.guest;
+
+import com.molveno.molveno.restaurant.reservation.Reservation;
+import com.molveno.molveno.restaurant.reservation.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GuestController {
+    @Autowired
+    GuestRepository guestRepository;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @RequestMapping(value = "/get-guest", method = RequestMethod.GET)
+    public List<Guest> getAll() {
+        return guestRepository.findAll();
+    }
+
+    @RequestMapping(value = "/save-guest", method = RequestMethod.POST , consumes="application/json")
+    public void saveGuest(@RequestBody Guest guest) {
+        guestRepository.save(guest);
+    }
+
+    @RequestMapping(value = "/delete-guest", method = RequestMethod.DELETE , consumes="application/json")
+    public void deleteReservation(@RequestBody Guest guest){
+        guestRepository.deleteById(guest.getId());
+
+    }
+
+
+
+
+}
