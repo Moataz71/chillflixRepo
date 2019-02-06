e5e0431bda984aaeea161a9ec6e721b5e61850dd
MALKURDI <mohmmad.alkurdi@capgemini.com>
2019-02-06 13:01:40 +0100

    update

--- src/main/java/com/molveno/molveno/restaurant/table/TableController.java
+++ src/main/java/com/molveno/molveno/restaurant/table/TableController.java
@@ -20,12 +20,24 @@ public class TableController {
         public List<Tablee> getAll() {
             return tableRepository.findAll();
         }
-        @RequestMapping(value = "/delete-table",method = RequestMethod.DELETE,consumes = "application/json")
+        @RequestMapping(value = "/delete-table",method = RequestMethod.DELETE)
     public void deleteTable(@RequestBody Tablee tablee){
             tableRepository.deleteById(tablee.getId());
 
 
         }
+
+    @RequestMapping(value = "/add-table", method = RequestMethod.POST,consumes = "application/json")
+    public void addTable(@RequestBody Tablee table) {
+        tableRepository.save(table);
+    }
+    @RequestMapping(value = "/edit-table", method = RequestMethod.POST,consumes = "application/json")
+    public void editTable(@RequestBody Tablee table) {
+        Tablee p = tableRepository.findById(table.getId());
+      //  p.setNameoftable(table.getNameoftable());
+        p.setNumberofchairs(table.getNumberofchairs());
+        tableRepository.save(p);
+    }
 
 
     }
