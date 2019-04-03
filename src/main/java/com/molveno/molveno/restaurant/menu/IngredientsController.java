78537e5657c6feca5c952916ef8c90a4f0a1ccf1
MALKURDI <mohmmad.alkurdi@capgemini.com>
2019-04-03 15:35:17 +0200

    menu front end

--- src/main/java/com/molveno/molveno/restaurant/menu/IngredientsController.java
+++ src/main/java/com/molveno/molveno/restaurant/menu/IngredientsController.java
@@ -61,6 +61,8 @@ public class IngredientsController {
             g.setPriceIngredients(ingredient.getPriceIngredients());
         }
 
+        g.setIngredientQuantityType(ingredient.getIngredientQuantityType());
+
         ingredientRepository.save(g);
 
 
